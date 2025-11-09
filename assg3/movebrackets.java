d830e6191bf0e62a52fbb2168ba36398b562426a
Anjali Pandey <anjalipandey52005@gmail.com>
2025-11-09 13:47:43 +0530

    move brackets edit

--- assg3/movebrackets.java
+++ assg3/movebrackets.java
@@ -2,48 +2,27 @@ package assg3;
 import java.util.*;
 public class movebrackets {
 
-    static int count=0;
-    public static void countMoves(char arr[],int i,int j){
-        if(isValid(arr)){
-            return;
+    public static int countMoves(String s,int i,int balance,int moves){
+        // base case
+        if(i==s.length()){
+            return moves;
         }
-        if(i>=j) {
-            return;
+        char curr = s.charAt(i);
+        if(curr=='('){
+            balance++;
         }
-        if(arr[i]==')' && arr[j]=='('){
-            char temp = arr[i];
-            arr[i]=arr[j];
-            arr[j] = temp;
-            count++;
+        else{
+            balance--;
         }
-        if(arr[i]=='('){
-            countMoves(arr, i+1, j);
+        if(balance<0){
+            moves++;
+            balance=0;
         }
-        if(arr[j]==')'){
-            countMoves(arr,i, j-1);
-        }
-    }
-    public static boolean isValid(char arr[]){
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==')' && st.empty()){
-                return false;
-            }
-            if(arr[i]=='('){
-                st.push(arr[i]);
-            }
-            else if(arr[i]==')'){
-                st.pop();
-            }
-        }
-        if(st.empty()) return true;
-
-        return false;
+        return countMoves(s, i+1, balance, moves);
     }
     public static void main(String[] args) {
         String str = ")))((((())";
-        char arr[] = str.toCharArray();
-        countMoves(arr,0,arr.length-1);
-        System.out.println(count);
+        int moves = countMoves(str,0,0,0);
+        System.out.println(moves);
     }
 }

1a0457de1265b886dde73752e8b56795417c262c
Anjali Pandey <anjalipandey52005@gmail.com>
2026-03-11 21:55:14 +0530

    frog jump

--- dp/frogjump.java
+++ dp/frogjump.java
@@ -1,25 +1,38 @@
-package dp;
-import java.util.Arrays;
+import java.util.*;
 
 public class frogjump {
-    public static int findCost(int arr[],int n,int dp[]){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return Math.abs(arr[1]-arr[0]);
-        }
+    // public static int findCost(int arr[],int n,int dp[]){
+    //     if(n==0){
+    //         return 0;
+    //     }
+    //     if(n==1){
+    //         return Math.abs(arr[1]-arr[0]);
+    //     }
         
+    //     if(dp[n]!=-1) return dp[n];
+    //     int cost1 = findCost(arr,n-1,dp) + Math.abs(arr[n]-arr[n-1]);   
+    //     int cost2 = findCost(arr,n-2,dp) + Math.abs(arr[n]-arr[n-2]);   
+    //     dp[n] = Math.min(cost1,cost2);
+    //     return dp[n];
+    // }
+
+    public static int jump(int arr[],int n,int dp[]){
+        if(n==1) return 0;
+        if(n==2) return Math.abs(arr[1]-arr[0]);
         if(dp[n]!=-1) return dp[n];
-        int cost1 = findCost(arr,n-1,dp) + Math.abs(arr[n]-arr[n-1]);   
-        int cost2 = findCost(arr,n-2,dp) + Math.abs(arr[n]-arr[n-2]);   
-        dp[n] = Math.min(cost1,cost2);
-        return dp[n];
+        int cost1 = jump(arr, n-1,dp) + Math.abs(arr[n-1]-arr[n-2]);
+        int cost2 = jump(arr, n-2,dp) + Math.abs(arr[n-1]-arr[n-3]);
+        return dp[n] = Math.min(cost1, cost2);
     }
     public static void main(String[] args) {
-        int height[] = {20,30,40,20};
-        int dp[] = new int[height.length]; 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int height[] = new int[n]; 
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+        int dp[] = new int[n+1]; 
         Arrays.fill(dp,-1);
-        System.out.println(findCost(height,height.length-1,dp));
+        System.out.println(jump(height,n,dp));
     }
 }
